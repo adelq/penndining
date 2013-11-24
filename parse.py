bbb9cd2b82bb05b933d61f3a1c023288c9509e84
David Lakata <dlakata@seas.upenn.edu>
2013-11-23 21:09:40 -0500

    Made functions to return meals dictionaries

--- parse.py
+++ parse.py
@@ -2,16 +2,6 @@ from bs4 import BeautifulSoup
 import urllib2
 import pprint
 
-url = urllib2.urlopen("http://cms.business-services.upenn.edu/dining/hours-locations-a-menus/residential-dining/hill-house/daily-menu.html")
-content = url.read()
-soup = BeautifulSoup(content)
-# print soup
-headers_raw = soup.findAll("h4")
-# print headers
-table_content = soup.findAll("table", {"class": "contentpaneopen"})
-# content = table_content[1].findAll("li")
-
-
 def listify(n):
 	"""
 	returns the food options for each category of food
@@ -35,33 +25,51 @@ def veg_listify(n):
 	output_list = raw.split("\n")
 	output_list = filter(None, output_list)
 	for i in output_list:
-		if "vegetarian" not in i or "vegan" not in i:
+		if "vegetarian" not in i and "vegan" not in i:
 			output_list.remove(i)
 	return output_list
 
-all_dict = {}
-for raw_header in headers_raw:
-	header_title = raw_header.getText()
-	temp = {}
-	for category in raw_header.next_sibling.findAll("strong"):
-		categoryText = category.getText()
-		temp[categoryText] = listify(category)
-	all_dict[header_title] = temp
+def get_all_meals(url):
+	url = urllib2.urlopen(url)
+	content = url.read()
+	soup = BeautifulSoup(content)
+	# print soup
+	headers_raw = soup.findAll("h4")
+	# print headers
+	table_content = soup.findAll("table", {"class": "contentpaneopen"})
+	# content = table_content[1].findAll("li")
+	all_dict = {}
+	for raw_header in headers_raw:
+		header_title = raw_header.getText()
+		temp = {}
+		for category in raw_header.next_sibling.findAll("strong"):
+			categoryText = category.getText()
+			temp[categoryText] = listify(category)
+		all_dict[header_title] = temp
+	return all_dict
 
-
-veg_dict = {}
-for raw_header in headers_raw:
-	header_title = raw_header.getText()
-	temp = {}
-	for category in raw_header.next_sibling.findAll("strong"):
-		categoryText = category.getText()
-		temp[categoryText] = veg_listify(category)
-	veg_dict[header_title] = temp
+def get_veg_meals(url):
+	url = urllib2.urlopen(url)
+	content = url.read()
+	soup = BeautifulSoup(content)
+	# print soup
+	headers_raw = soup.findAll("h4")
+	# print headers
+	table_content = soup.findAll("table", {"class": "contentpaneopen"})
+	# content = table_content[1].findAll("li")
+	veg_dict = {}
+	for raw_header in headers_raw:
+		header_title = raw_header.getText()
+		temp = {}
+		for category in raw_header.next_sibling.findAll("strong"):
+			categoryText = category.getText()
+			temp[categoryText] = veg_listify(category)
+		veg_dict[header_title] = temp
 
 # print all_dict
 pp = pprint.PrettyPrinter(indent = 2)
-pp.pprint(all_dict)
+pp.pprint(get_all_meals("http://cms.business-services.upenn.edu/dining/hours-locations-a-menus/residential-dining/hill-house/daily-menu.html"))
 
-print all_dict["BRUNCH"]["Pizza"]
+# print all_dict["BRUNCH"]["Pizza"]
 
-pp.pprint(veg_dict)
+# pp.pprint(veg_dict) "http://cms.business-services.upenn.edu/dining/hours-locations-a-menus/residential-dining/hill-house/daily-menu.html"
