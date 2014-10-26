1f1260e868d62b2ece2a7bb4678bca8ab13ccca1
Adel Qalieh <aqalieh95@gmail.com>
2014-10-26 18:01:31 -0400

    Refactor requests to use hall url list

--- old.py
+++ old.py
@@ -13,18 +13,18 @@ HALL_URLS = [
 @bpp.route('/old/')
 @bpp.route('/<hill>/<commons>/<kings>/<mcclelland>/')
 def index(hill=None, commons=None, kings=None, mcclelland=None):
-    hill = parse.get_all_meals("http://cms.business-services.upenn.edu/dining/hours-locations-a-menus/residential-dining/hill-house/daily-menu.html")
-    commons = parse.get_all_meals("http://cms.business-services.upenn.edu/dining/hours-locations-a-menus/residential-dining/1920-commons/1920-menu.html")
-    kings = parse.get_all_meals("http://cms.business-services.upenn.edu/dining/hours-locations-a-menus/residential-dining/english-house/daily-menu.html")
-    mcclelland = parse.get_all_meals("http://cms.business-services.upenn.edu/dining/hours-locations-a-menus/residential-dining/mcclelland-express/daily-menu.html")
+    hill = parse.get_all_meals(HALL_URLS[0])
+    commons = parse.get_all_meals(HALL_URLS[1])
+    kings = parse.get_all_meals(HALL_URLS[2])
+    mcclelland = parse.get_all_meals(HALL_URLS[3])
     return render_template('default.html', hill=hill, commons=commons, kings=kings, mcclelland=mcclelland)
 
 
 @bpp.route('/')
 @bpp.route('/<hill>/<commons>/<kings>/<mcclelland>/')
 def default(hill=None, commons=None, kings=None, mcclelland=None):
-    hill = parse.get_all_meals("http://cms.business-services.upenn.edu/dining/hours-locations-a-menus/residential-dining/hill-house/daily-menu.html")
-    commons = parse.get_all_meals("http://cms.business-services.upenn.edu/dining/hours-locations-a-menus/residential-dining/1920-commons/1920-menu.html")
-    kings = parse.get_all_meals("http://cms.business-services.upenn.edu/dining/hours-locations-a-menus/residential-dining/english-house/daily-menu.html")
-    mcclelland = parse.get_all_meals("http://cms.business-services.upenn.edu/dining/hours-locations-a-menus/residential-dining/mcclelland-express/daily-menu.html")
+    hill = parse.get_all_meals(HALL_URLS[0])
+    commons = parse.get_all_meals(HALL_URLS[1])
+    kings = parse.get_all_meals(HALL_URLS[2])
+    mcclelland = parse.get_all_meals(HALL_URLS[3])
     return render_template('default.html', hill=hill, commons=commons, kings=kings, mcclelland=mcclelland)
